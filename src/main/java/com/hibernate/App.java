dc8667190b8ffb0aa7d60720a6cd618340a901a1
Andrels <alspersonalmail@gmail.com>
2023-04-14 20:15:46 -0300

    IGNORANDO ARQUIVOS

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,18 +1,14 @@
 package com.hibernate;
 
-import java.util.List;
-
-/* import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration; */
+import org.hibernate.cfg.Configuration; 
 
 import com.hibernate.models.User;
 
 import jakarta.persistence.*;
 
-/* import java.util.ArrayList;
+ import java.util.*;
 
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
@@ -25,6 +21,7 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
@@ -39,7 +36,7 @@ import models.User; */
 public class App {
     public static void main(String[] args) {
         //CONNECTION WITH MONGODB
-       /*  ArrayList<?> databases = new ArrayList<>();
+         ArrayList<?> databases = new ArrayList<>();
         MongoDatabase database = null;
         MongoCollection<User> collection = null;
         MongoClientConnection con = new MongoClientConnection();
@@ -47,7 +44,7 @@ public class App {
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                 fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-        /* MongoClientSettings settings = MongoClientSettings.builder()
+         MongoClientSettings settings = MongoClientSettings.builder()
                 .codecRegistry(pojoCodecRegistry)
                 .build();
         MongoClient mongoClientSettings = MongoClients.create(settings); 
@@ -60,9 +57,9 @@ public class App {
             database = connection.getDatabase("hibernatedb");
             collection = database.getCollection("users", User.class);
             collection = collection.withCodecRegistry(pojoCodecRegistry);
-            //long size = collection.countDocuments();
-            //User user = new User(size,"Silva");
-            //collection.insertOne(user);
+            long size = collection.countDocuments();
+            User user = new User(size,"Silva");
+            collection.insertOne(user);
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -70,11 +67,11 @@ public class App {
         System.out.println();
         System.out.println();
         System.out.println();
-        //con.closeConnection();
+        con.closeConnection();
          try {
             Bson filter = Filters.eq("_id", 3);
             FindIterable<User> users = collection.find(filter);
-            /* for(User u : users){
+             for(User u : users){
                 System.out.println("USERS: " + u);
             }
             users.forEach(user -> {
@@ -83,12 +80,12 @@ public class App {
 
         }catch(Exception e){
             e.printStackTrace();
-        }  */
-       
+        }  
+    //**********8*****************************************************************8 */   
 
         //CONNECTIO WITH MYSQL
         //COMING FROM  hibernate.cfg.xml
-        /* SessionFactory sessionFactory = new Configuration()
+         SessionFactory sessionFactory = new Configuration()
         .configure("resources/hibernate.cfg.xml")
         .addAnnotatedClass(User.class)
         .buildSessionFactory();
@@ -98,7 +95,7 @@ public class App {
             session.beginTransaction();
             User user = new User(null, "Jababa Luis");
             List<User> users = session.createQuery("from user where name LIKE '%Luis%'", User.class).list();
-            /* List<User> users = session.createQuery("from user where name LIKE '%Luis'", User.class).list();
+            List<User> users = session.createQuery("from user where name LIKE '%Luis'", User.class).list();
             List<User> users = session.createQuery("from user where name LIKE 'Luis%'", User.class).list(); 
             session.get(User.class, user);
             for(User u : users){
@@ -109,12 +106,12 @@ public class App {
             e.printStackTrace();       
         }finally{
             sessionFactory.close();
-        } */
-
+        } 
+ /*****************************************************************************************************8 */
 
         //USING JPA
         //Coming from persistence.xml
-       /*  EntityManagerFactory emf = Persistence.createEntityManagerFactory("cinema");
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("cinema");
         EntityManager em = emf.createEntityManager();
 
         TypedQuery<User> users = em.createNamedQuery("getAllUsers", User.class);
@@ -125,6 +122,6 @@ public class App {
             System.out.println(user);
         });
 
-        em.close(); */
+        em.close(); 
 } 
 }
